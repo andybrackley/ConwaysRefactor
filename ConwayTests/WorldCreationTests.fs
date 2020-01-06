1f43275b0966a3663e08cf48be506b81e400841c
AndyBrackley <andybrackley@gmail.com>
2020-01-06 23:26:03 +0000

    Added tests for the evolve method

--- ConwayTests/WorldCreationTests.fs
+++ ConwayTests/WorldCreationTests.fs
@@ -1,44 +1,29 @@
-module ConwayTests
+namespace ConwayTests
 
 open NUnit.Framework
 
-module SampleData = 
-    let input = """
-_____
-__*__
-_***_
-__*__
-_____
-"""
+module WorldCreation = 
+    [<Test>]
+    let ``Given the sample input the world is created as expected`` () =
+        let expected = [
+            [ false; false; false; false; false ]
+            [ false; false; true; false; false ]
+            [ false; true; true; true; false ]
+            [ false; false; true; false; false ]
+            [ false; false; false; false; false ]
+        ]
 
-    let world = Program.createWorldFromString input 
+        CollectionAssert.AreEquivalent(expected, SampleData.world)
 
+    [<Test>]
+    let ``world can be retrieved as string array``() = 
+        let expected = [
+           "     "
+           "  *  "
+           " *** "
+           "  *  "
+           "     "
+        ]
 
-[<SetUp>]
-let Setup () =
-    ()
-
-[<Test>]
-let ``Given the sample input the world is created as expected`` () =
-    let expected = [
-        [ false; false; false; false; false ]
-        [ false; false; true; false; false ]
-        [ false; true; true; true; false ]
-        [ false; false; true; false; false ]
-        [ false; false; false; false; false ]
-    ]
-
-    CollectionAssert.AreEquivalent(expected, SampleData.world)
-
-[<Test>]
-let ``world can be retrieved as string array``() = 
-    let expected = [
-       "     "
-       "  *  "
-       " *** "
-       "  *  "
-       "     "
-    ]
-
-    let dump = Program.getWorldAsText SampleData.world
-    CollectionAssert.AreEquivalent(expected, dump)
+        let dump = Program.getWorldAsText SampleData.world
+        CollectionAssert.AreEquivalent(expected, dump)
---
+++ ConwayTests/WorldEvolveTests.fs
@@ -0,0 +1,19 @@
+﻿namespace ConwayTests
+
+open NUnit.Framework
+
+module WorldEvolveTests = 
+   [<Test>]
+   let ``On evolve new world is created``() = 
+      let world = SampleData.world
+      let evolved = Program.evolve world
+      let expected = [
+         [ false; false; false; true; false; ]
+         [ false; false; false; false; false; ]
+         [ false; false; false; false; false; ]
+         [ false; false; false; false; false; ]
+         [ false; false; false; false; false; ]
+      ]
+
+      CollectionAssert.AreEquivalent(expected, world)
+
