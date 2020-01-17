d12a297d80a034e7bb3298845d8e896b9f6136fc
AndyBrackley <andybrackley@gmail.com>
2020-01-17 15:04:49 +0000

    Initial Attempt at restructuring the algorithm to make it more scalable.

--- ConwayTests/WorldEvolveTests.fs
+++ ConwayTests/WorldEvolveTests.fs
@@ -98,7 +98,7 @@ module WorldEvolveTests =
    [<Test>]
    let ``On evolve new world is created``() = 
       let world = SampleData.world()
-      let evolved = Program.Generations.evolve world
+      let evolved = Program.Generations.evolveWorld world
       let expected = [
          [ Dead; Dead; Dead; Dead; Dead; ]
          [ Dead; Alive; Alive; Alive; Dead; ]
@@ -107,60 +107,45 @@ module WorldEvolveTests =
          [ Dead; Dead; Dead; Dead; Dead; ]
       ]
 
-      CollectionAssert.AreEquivalent(expected, evolved.Grid)
+      CollectionAssert.AreEquivalent(expected, evolved |> World.getAs2dArray)
 
 
    [<TestCaseSource(typedefof<SampleInputsProvider>, "Samples")>]
    let ``evolve matches gameoflife implementation for various shapes``(sampleInput) =
       let world = World.createFromStringArray sampleInput
-      let newWorld = Generations.evolve world
-      World.getAsText newWorld
+      world 
+        |> World.getAsText
+        |> Seq.iter(fun l -> System.Diagnostics.Debug.WriteLine(sprintf "%s" l))
+
+      let newWorld = Generations.evolveWorld world
+      let asText = World.getAsText newWorld
+      asText |> Seq.iter(fun l -> System.Diagnostics.Debug.WriteLine(sprintf "%s" l))
+      asText
 
    [<Test>]
    let ``retrieving coordinates of neighbours gives expected results``() =
        let expected = [
-          { x = 0; y = 0 };
-          { x = 0; y = 1 };
-          { x = 0; y = 2 };
-          { x = 1; y = 0 };
-          { x = 1; y = 2 };
-          { x = 2; y = 0 };
-          { x = 2; y = 1 };
-          { x = 2; y = 2 };
+          (0, 0);
+          (0, 1);
+          (0, 2);
+          (1, 0);
+          (1, 2);
+          (2, 0);
+          (2, 1);
+          (2, 2);
        ]
 
-       let neighbours = Generations.getNeighbourCoords { x = 1; y = 1 }
+       let expected = expected |> List.map(fun (r, c) -> { x = Row(r); y = Col(c) })
+
+       let world = SampleData.world()
+       let neighbours = Generations.getNeighbourCoords world { x = Row(1); y = Col(1) }
        CollectionAssert.AreEquivalent(expected, neighbours)
-
-   [<Test>]
-   let ``Sanitize Coordinates Loops From 0 to Height``() =
-       let c = Generations.sanitizeCoordinate { x = -1; y = 0 } 5 5
-       Assert.That(c.x, Is.EqualTo(4))
-       Assert.That(c.y, Is.EqualTo(0))
-
-   [<Test>]
-   let ``Sanitize Coordinates Loops From Height to 0``() =
-       let c = Generations.sanitizeCoordinate { x = 5; y = 0 } 5 5
-       Assert.That(c.x, Is.EqualTo(0))
-       Assert.That(c.y, Is.EqualTo(0))
-
-   [<Test>]
-   let ``Sanitize Coordinates Loops From 0 to Width``() =
-       let c = Generations.sanitizeCoordinate { x = 0; y = -1 } 5 5
-       Assert.That(c.x, Is.EqualTo(0))
-       Assert.That(c.y, Is.EqualTo(4))
-
-   [<Test>]
-   let ``Sanitize Coordinates Loops From Width to 0``() =
-       let c = Generations.sanitizeCoordinate { x = 0; y = 5 } 5 5
-       Assert.That(c.x, Is.EqualTo(0))
-       Assert.That(c.y, Is.EqualTo(0))
 
    [<Test>]
    let ``getAliveNeighbourCount returns count of only alive cells``() =
       let world = SampleData.world()
-      let coord = { x = 1; y = 3 }
-      let aliveNeighbourCount = Generations.getAliveNeighbourCount world 5 5 coord
+      let coord = { x = Row(1); y = Col(3) }
+      let aliveNeighbourCount = Generations.getAliveNeighbourCount world coord
       Assert.That(aliveNeighbourCount, Is.EqualTo(3))
 
    [<TestCase(1, ExpectedResult = false)>]
